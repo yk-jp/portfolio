3bcc9de14bc536c14cd81546ef72b44e5936c3c1
yusuke <ID+username@users.noreply.github.com>
2021-09-06 00:39:53 +0900

    setup for react environment with webpack is done.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import style from './style.module.css';
+const App = () => {
+  return (
+    <div>Hello world</div>
+  );
+}
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+module.exports = {
+  entry: "./src/index.tsx",
+
+  output: {
+    filename: "./bundle.js",
+    path: path.resolve(__dirname, 'build')
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'jsx', '.json']
+  },
+
+  devServer: {
+    static: './build',
+    port: 3000
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
+        ],
+        include: /\.module\.css$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+        exclude: /\.module\.css$/
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new Dotenv()
+  ]
+}

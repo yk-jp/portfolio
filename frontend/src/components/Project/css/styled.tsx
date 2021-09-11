91be337a51df3504319481d2f6c22165487186bb
yusuke <ID+username@users.noreply.github.com>
2021-09-12 00:22:52 +0900

    created all layout. #4 #5

--- frontend/src/components/Project/css/styled.tsx
+++ frontend/src/components/Project/css/styled.tsx
@@ -12,26 +12,86 @@ export const ProjectmainImgDiv = styled.div`
   width:100%;
 `;
 
-export const ImgTitleSpan = styled.h3`
+export const ImgTitleSpan = styled.p`
+  width:60vw;
   position:absolute;
   top:50%;
+  left:50%;
+  margin:0;
+  padding:0;
+  font-size:1em;
+  --ms-transform: translate(-50%,-50%);
+  --webkit-transform: translate(-50%,-50%);
+  transform: translate(-50%,-50%);
+  overflow-wrap: break-word;
+  @media(min-width:768px) { 
+    width:17vw;
+  }
+`;
+
+export const MoreInfoButton = styled.button`
+  width:15vw;
+  height:5vh;
+  position:absolute;
+  font-size:0.6em;
+  top:80%;
   left:50%;
   margin:0;
   padding:0;
   --ms-transform: translate(-50%,-50%);
   --webkit-transform: translate(-50%,-50%);
   transform: translate(-50%,-50%);
+
+  background-color:transparent;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  @media(min-width:768px) { 
+    width:10vw;
+    height:5vh;
+    font-size:1em;
+  }
 `;
 
 export const Img = styled.img`
-  opacity: 0.5;
+  opacity:0.5;
   width:80vw;
-  height:20vh;
+  height:auto;
 
    /* laptop */
    @media(min-width:768px) { 
     width:21vw;
-    height:25vh;
+    height:20vh;
   }
 `;
 
+export const ModalContainer = styled.div`
+  width:80vw;
+  height:auto;
+  /* laptop */
+  @media(min-width:768px) { 
+    width:45vw;
+  }
+
+  // desktop
+  @media (min-width: 1200px) {
+    width:35vw;
+  } 
+`;
+
+
+const ModalContentDefault = styled.div`
+  display:flex;
+  flex-direction:column;
+`;
+
+export const ModalContentDiv = styled(ModalContentDefault)``;
+
+
+export const ModalContentText = styled(ModalContentDefault)``;
+
+export const ModalChipDiv = styled.div`
+    display:flex;
+    flex-wrap:wrap;
+`;

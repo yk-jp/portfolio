5f6b0da4795d8c5327adf8feb5ea41ffc352b554
yusuke <ID+username@users.noreply.github.com>
2021-10-15 11:05:03 +0900

    added new Item

--- frontend/src/assets/img/Img.ts
+++ frontend/src/assets/img/Img.ts
@@ -2,12 +2,14 @@ import codenames from './codenames.png';
 import blackjack from './blackjack.png';
 import cli from './cli.png';
 import portfolio from './portfolio.png';
+import passwordmanager from './passwordmanager.png';
 
 const Img = {
   codenames: codenames,
   blackjack: blackjack,
   cli: cli,
-  portfolio: portfolio
+  portfolio: portfolio,
+  passwordmanager:passwordmanager
 }
 
 export default Img;

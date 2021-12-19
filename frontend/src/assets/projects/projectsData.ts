fcb505b8152d452509d2b0f168bfcae6a5b1d197
yusuke <ID+username@users.noreply.github.com>
2021-12-19 11:39:28 -0800

    added info for passwordmanager

--- frontend/src/assets/projects/projectsData.ts
+++ frontend/src/assets/projects/projectsData.ts
@@ -56,9 +56,9 @@ const projectsData: IProject[] = [{
     "description": "This is a web app to manage your password.",
     "whatILearnt": "I was able to learn the basic authentication system. This is my first authentication project. I was happy to grash the understandings of where to store and manage both token(e.g. cookie or local storage in frontend), how privacy data should be more secure and why exactly JWT is used.",
     "projectDifficulties": "I couldn't grasp how refresh token works and how each request and response has to be authorized at first. I searched quite a lot of articles on the internet and learnt from those resorces enough. In addition to those things, I checked the system in a frontend side,backend side separately. In the backend side, I especially utilized a rest api testing quite enough. I finally was able to see the big picture.",
-    "techStack": ["HTML", "CSS", "Bootstrap", "React", "TypeScript","Node.js","Postman","Jest","Redis","MySQL","JWT"],
+    "techStack": ["HTML", "CSS", "Bootstrap", "React", "TypeScript","Node.js","Postman","Jest","Redis","MySQL","JWT","PM2"],
     "link": {
-        "visitWebsite": null,
+        "visitWebsite": "http://password-manager-livid.vercel.app/",
         "gitHub": "https://github.com/yk-jp/PasswordManager"
     }
 }

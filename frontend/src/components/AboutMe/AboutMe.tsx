9b61b73d8616644980a543cb9984210c2bd939b0
yusuke <ID+username@users.noreply.github.com>
2021-12-19 09:50:06 -0800

    changed jobtitle

--- frontend/src/components/AboutMe/AboutMe.tsx
+++ frontend/src/components/AboutMe/AboutMe.tsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
       <AboutMeTextDiv>
         <h4 id="AboutMe" className="white-text center-align margin-bottom-30px orange-text text-darken-1">About Me</h4>
         <div className="container">
-          <h5 className="white-text center-align flow-text">Hello, I'm Yusuke. I'm a Backend Developer, having a strong passion for learning technologies, working on projects. Check out my projects if you like :)</h5>
+          <h5 className="white-text center-align flow-text">Hello, I'm Yusuke. I'm a Full Stack Developer, having a strong passion for learning technologies, working on projects. Check out my projects if you like :)</h5>
         </div>
       </AboutMeTextDiv>
     </AboutMeContainer>
--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         {/* self-info text */}
         <SelfIntroTextDiv>
           <h2 className="white-text center-align">Yusuke</h2>
-          <h4 className="white-text center-align">Backend Developer</h4>
+          <h4 className="white-text center-align">Full Stack Developer</h4>
         </SelfIntroTextDiv>
         {/* social accounts */}
         <SocialAccounts />

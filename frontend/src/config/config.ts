295aa6d2537ef8097df7e10a5395d1579f247f4e
yusuke <k82.y12@gmail.com>
2022-03-19 11:02:42 -0700

    delete twitter link

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,17 +1,15 @@
-const twitter: string = process.env.TWITTER;
 const linkedIn: string = process.env.LINKEDIN;
 const gitHub: string = process.env.GITHUB;
 const email: string = process.env.EMAIL;
 
 const privateInfo = {
-  twitter: twitter,
   linkedIn: linkedIn,
   gitHub: gitHub,
-  email: email
+  email: email,
 };
 
 const config = {
-  privateInfo: privateInfo
-}
+  privateInfo: privateInfo,
+};
 
 export default config;
